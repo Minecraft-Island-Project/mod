9fee80ef3cb0d956e3f5b9df6df30a9ab510e886
macuguita <raulpripri@gmail.com>
2026-01-04 05:27:56 +0100

    made config stuff not mutable

--- src/main/kotlin/com/macuguita/island/server/ServerConfig.kt
+++ src/main/kotlin/com/macuguita/island/server/ServerConfig.kt
@@ -10,21 +10,21 @@ import folk.sisby.kaleido.lib.quiltconfig.api.annotations.Comment
 class ServerConfig : WrappedConfig() {
 
     @Comment("The message that shows up when a player first joins the server")
-    val greetingMessage: String = "%s has joined for the first time, say hi!"
+    var greetingMessage: String = "%s has joined for the first time, say hi!"
 
     @Comment("Configurations for the minecraft chat -> discord server connection")
-    val discord = Discord()
+    var discord = Discord()
     class Discord : Section {
         @Comment("Default webhook name")
-        val serverName: String = "Island"
+        var serverName: String = "Island"
 
         @Comment("Webhook URL")
-        val discordWebhookUrl: String = "PLACE_YOUR_WEBHOOK"
+        var discordWebhookUrl: String = "PLACE_YOUR_WEBHOOK"
 
         @Comment("Head api url, the %s should be the slot for a player uuid")
-        val headApiUrl: String = "https://mc-heads.net/avatar/%s"
+        var headApiUrl: String = "https://mc-heads.net/avatar/%s"
 
         @Comment("Url to image to show for default announcements")
-        val defaultImage: String = "https://avatars.githubusercontent.com/u/251938043"
+        var defaultImage: String = "https://avatars.githubusercontent.com/u/251938043"
     }
 }
